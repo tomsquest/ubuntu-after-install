163f3b23fbde232a5f09ebec1704a64a6629f7a1
Thomas Queste <tom@tomsquest.com>
2016-06-03 08:52:17 +0200

    Auto say yes for adding ppas and installing pkgs

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 echo "#### Adding PPAs"
 while read ppa; do
-sudo add-apt-repository $ppa
+sudo add-apt-repository --yes $ppa
 done < ppas.txt
 
 echo "#### Installing packages"
 sudo apt-get update
 declare -a packages
 readarray packages < packages.txt
-sudo apt-get install ${packages[*]}
+sudo apt-get install --yes ${packages[*]}
 
 echo "#### Configure grub to use console mode"
 sudo patch -p0 < grub-config.patch
